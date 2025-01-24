c775a20593f32e4c4d203a8a55dfbac54f0d2e57
Kumar Sai Uggina <uggina.kumarsai@gmail.com>
2025-01-25 01:42:14 +0530

    Added Dockerfile

--- backend/src/app.js
+++ backend/src/app.js
@@ -28,7 +28,7 @@ const startServer = async () => {
 
   // Start the server
   const PORT = process.env.PORT || 5000;
-  app.listen(PORT, () => {
+  app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server is running on port ${PORT}`);
   });
 };
